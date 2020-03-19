b9486fd091e0942905f1b4dcfde104a0d675c60e
Mario Lang <mariolang1@gmx.de>
2020-03-19 15:34:24 +0100

    added client chat

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ChatService } from './services/chat.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+	title = 'client';
+
+	constructor(private chat: ChatService) { }
 }
