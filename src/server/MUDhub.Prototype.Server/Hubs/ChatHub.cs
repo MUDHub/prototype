7cae0b48dfaebb3332d338ca2673aec21c318270
maSchoeller <git@marvinschoeller.de>
2020-03-19 15:30:02 +0100

    update

--- src/server/MUDhub.Prototype.Server/Hubs/ChatHub.cs
+++ src/server/MUDhub.Prototype.Server/Hubs/ChatHub.cs
@@ -9,9 +9,9 @@ namespace MUDhub.Prototype.Server.Hubs
 {
     public class ChatHub  : Hub
     {
-        public Task SendMessage(string user, string name)
+        public Task SendMessage(string user, string message)
         {
-            return Clients.All.SendAsync("receiveMessage", user, name);
+            return Clients.All.SendAsync("receiveMessage", user, message);
         }
     }
 }
