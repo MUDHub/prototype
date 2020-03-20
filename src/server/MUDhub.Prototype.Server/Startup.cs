2a3908a89b81978670ab53bcef7307f92877677f
maSchoeller <git@marvinschoeller.de>
2020-03-20 13:09:47 +0100

    update

--- src/server/MUDhub.Prototype.Server/Startup.cs
+++ src/server/MUDhub.Prototype.Server/Startup.cs
@@ -53,13 +53,13 @@ namespace MUDhub.Prototype.Server
                 endpoints.MapHub<ChatHub>("/chat");
             });
 
-            //app.UseSpa(spa =>
-            //{
-            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //    // see https://go.microsoft.com/fwlink/?linkid=864501
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
 
-            //    spa.Options.SourcePath = Configuration["spaDestination"];
-            //});
+                spa.Options.SourcePath = Configuration["spaDestination"];
+            });
         }
     }
 }
