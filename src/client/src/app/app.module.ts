3329e6d0a15655a2bd12dcce627a9355d0c666aa
Paul Finkbeiner <paul-d-f@web.de>
2020-03-25 15:48:14 +0100

    login layout finished

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -5,24 +5,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
 import { LoginComponent } from './login/login.component';
-import { GlobalChatComponent } from './game/global-chat/global-chat.component';
 import { ContainerComponent } from './container/container.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		GameComponent,
 		LoginComponent,
-		GlobalChatComponent,
 		ContainerComponent
 	],
 	imports: [
 		BrowserModule,
 		ReactiveFormsModule,
 		HttpClientModule,
-		AppRoutingModule
+		AppRoutingModule,
+		FormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
