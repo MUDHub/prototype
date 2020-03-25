6ce21782b868408ebec9afcf27783e4fd84b1672
Mario Lang <mariolang1@gmx.de>
2020-03-25 14:52:44 +0100

    removed old code

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
