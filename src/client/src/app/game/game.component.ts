b0c3c27386d9a80b58df90fd51fe2a6c4d62fada
Mario Lang <mariolang1@gmx.de>
2020-03-25 14:51:22 +0100

    changed indentation

--- src/client/src/app/game/game.component.ts
+++ src/client/src/app/game/game.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+	selector: 'app-game',
+	templateUrl: './game.component.html',
+	styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
