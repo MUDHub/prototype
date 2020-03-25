66d4e1fda34e52f52492d8052834967d2e7eaa87
Mario Lang <mariolang1@gmx.de>
2020-03-25 16:36:51 +0100

    implemented chat logic

--- src/client/src/app/global-chat/global-chat.component.spec.ts
+++ src/client/src/app/global-chat/global-chat.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GlobalChatComponent } from './global-chat.component';
 
 describe('GlobalChatComponent', () => {
-  let component: GlobalChatComponent;
-  let fixture: ComponentFixture<GlobalChatComponent>;
+	let component: GlobalChatComponent;
+	let fixture: ComponentFixture<GlobalChatComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GlobalChatComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ GlobalChatComponent ]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GlobalChatComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(GlobalChatComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
