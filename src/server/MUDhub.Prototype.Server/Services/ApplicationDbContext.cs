6d8c84600477fa4a074ba4be16dfadab3dc15e15
maSchoeller <git@marvinschoeller.de>
2020-03-26 07:48:46 +0100

    fix applicationcontext

--- src/server/MUDhub.Prototype.Server/Services/ApplicationDbContext.cs
+++ src/server/MUDhub.Prototype.Server/Services/ApplicationDbContext.cs
@@ -12,49 +12,35 @@ namespace MUDhub.Prototype.Server.Services
     public class ApplicationDbContext : DbContext
     {
 
-        public List<User> List { get; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            List = new List<User>();
             Database.EnsureDeleted();
-            //Database.Migrate();
             Database.EnsureCreated();
-            if (Users.FirstOrDefault() is null)
-            {
-                CreateInitalUsers();
-            }
-
         }
 
-        private void CreateInitalUsers()
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            var user = new User
+            modelBuilder.Entity<User>().HasData(new User
             {
                 Id = Guid.NewGuid().ToString(),
                 Username = "marvin",
                 PasswordHash = UserManager.CreatePasswordHash("hallowelt")
-            };
-            List.Add(user);
-            Users.Add(user);
-            user = new User
+            });
+            modelBuilder.Entity<User>().HasData(new User
             {
                 Id = Guid.NewGuid().ToString(),
                 Username = "mario",
                 PasswordHash = UserManager.CreatePasswordHash("password")
-            };
-            List.Add(user);
-            Users.Add(user);
-            user = new User
+            });
+            modelBuilder.Entity<User>().HasData(new User
             {
                 Id = Guid.NewGuid().ToString(),
                 Username = "moris",
                 PasswordHash = UserManager.CreatePasswordHash("test")
-            };
-            List.Add(user);
-            Users.Add(user);
-            SaveChanges();
+            });
+
         }
         public DbSet<User> Users { get; set; } = null!;
-        public DbSet<Room> Rooms { get; set; } = null!;    
+        public DbSet<Room> Rooms { get; set; } = null!;
     }
 }
