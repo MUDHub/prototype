ba2e51eaad6575af39e1ec861473102799cde809
maSchoeller <git@marvinschoeller.de>
2020-03-26 09:56:26 +0100

    update

--- src/server/MUDhub.Prototype.Server/Startup.cs
+++ src/server/MUDhub.Prototype.Server/Startup.cs
@@ -96,23 +96,23 @@ namespace MUDhub.Prototype.Server
                 // See https://docs.microsoft.com/aspnet/core/signalr/security#access-token-logging
                 // for more information about security considerations when using
                 // the query string to transmit the access token.
-                x.Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var accessToken = context.Request.Query["access_token"];
+                //x.Events = new JwtBearerEvents
+                //{
+                //    OnMessageReceived = context =>
+                //    {
+                //        var accessToken = context.Request.Query["access_token"];
 
-                        // If the request is for our hub...
-                        var path = context.HttpContext.Request.Path;
-                        if (!string.IsNullOrEmpty(accessToken) &&
-                            (path.StartsWithSegments("/hubs")))
-                        {
-                            // Read the token out of the query string
-                            context.Token = accessToken;
-                        }
-                        return Task.CompletedTask;
-                    }
-                };
+                //        // If the request is for our hub...
+                //        var path = context.HttpContext.Request.Path;
+                //        if (!string.IsNullOrEmpty(accessToken) &&
+                //            (path.StartsWithSegments("/hubs")))
+                //        {
+                //            // Read the token out of the query string
+                //            context.Token = accessToken;
+                //        }
+                //        return Task.CompletedTask;
+                //    }
+                //};
             });
 
             services.AddSwaggerGen(c =>
